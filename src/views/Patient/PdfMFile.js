8778d036354a7681d1bc9279eaf7db86eafd39f9
Feriel Hakim <feriel.hakim@esprit.tn>
2022-05-08 17:59:45 +0100

    pdf styling

--- src/views/Patient/PdfMFile.js
+++ src/views/Patient/PdfMFile.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
-import  ReactPDF,{ Document, Page, Text, View, StyleSheet ,PDFViewer } from '@react-pdf/renderer';
+import  ReactPDF,{ Document, Page, Text, Image,View, StyleSheet ,PDFViewer } from '@react-pdf/renderer';
 import MedicalFileView from "./medicalFileView";
-
+import logoNav1 from "../../assets/img/logoNav.png"
+import {blue, lightBlue} from "@mui/material/colors";
 const styles = StyleSheet.create({
     page: {
         flexDirection: 'row',
@@ -11,6 +12,36 @@ const styles = StyleSheet.create({
         margin: 10,
         padding: 10,
         flexGrow: 1
+    },
+    image:{
+        width: 200,
+        marginVertical:10,
+        marginHorizontal:180,
+        textAlign: "Centre"
+
+    },
+    text:{
+        margin : 12,
+        fontSize:14,
+        textAlign:"Justify",
+        fontFamily:"Times-Roman"
+
+    },
+    title:{
+        color: '#007bff',
+        marginVertical:10,
+        marginHorizontal:200,
+        fontSize:24,
+        textAlign: "Centre"
+
+    },
+    footer:{
+        color: '#007bff',
+        marginVertical:10,
+        marginHorizontal:215,
+        fontSize:14,
+        textAlign: "Centre"
+
     }
 });
 function PdfMFile (props)  {
@@ -23,9 +54,13 @@ function PdfMFile (props)  {
         <Document>
             <Page size="A4" style={styles.page}>
                 <View style={styles.section}>
-                    <Text>
+                    <Image style={styles.image} src={logoNav1} />
+                    <Text style={styles.title} > Medical File</Text>
+                    <Text style={styles.text} >
+
                         <MedicalFileView  medicalFile={MedicalFile} ></MedicalFileView>
                     </Text>
+                    <Text style={styles.footer} > *** Thank You ***</Text>
                 </View>
             </Page>
         </Document>
@@ -34,3 +69,4 @@ function PdfMFile (props)  {
 };
 
 export default PdfMFile;
+
